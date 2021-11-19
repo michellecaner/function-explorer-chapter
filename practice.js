f5224a1751ac15288c0ea1e994fecbb89635e1e5
michellecaner <michellecaner@gmail.com>
2021-11-18 21:27:36 -0600

    initial commit

---
+++ practice.js
@@ -0,0 +1,48 @@
+// const calculateAgeInDogYears = (age) => {
+// const ageInDogYears = age / 7
+
+// return ageInDogYears
+// }
+
+// const dogAge = calculateAgeInDogYears(22)
+
+// console.log(dogAge)
+
+
+
+// const bestInShow = (dogName) => {
+// if (dogName === "Schnauzer") {
+//   return " my favorite dog breed is Schnauzer."
+//   } else if (dogName === "meow") {
+//     return " I like cats."
+//   } else {
+//     return " just give me whatever you got!"
+//   }
+// }
+
+// let myFavorite = bestInShow("lizard")
+
+// console.log("When it comes to pets," + myFavorite);
+
+
+
+// const add = (num1, num2, num3) => {
+//   return num1 + num2 + num3
+// } 
+
+// console.log(add(17, 4, 11))
+
+// const go = (direction, speed) => {
+//   if (speed > 75) {
+//     return `The car is moving ${direction} at ${speed} mph. SLOW DOWN!`
+//   } else {
+//     return `The car is moving ${direction} at ${speed} mph.`
+//   }
+// }
+
+// console.log(go("forwards", 82))
+
+
+const evenOrOdd = (num1) => {
+  if (num1 === )
+}
